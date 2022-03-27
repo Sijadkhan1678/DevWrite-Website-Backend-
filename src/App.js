0cd70a677241e8ec42ac49227dfb1aa63f2bd708
sijadkhan1678@gmail.com <sijadkhan1678@gmail.com>
2022-03-27 11:10:32 +0500

    connected redux to the application and available store globally

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import {BrowserRouter as Router ,Route,Switch}  from 'react-router-dom';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import store from './store'
+import store from './store';
+import {Provider} from 'react-redux';
 import Home from './components/pages/Home'
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
@@ -16,7 +17,8 @@ const App= ()=> {
   }, []);
 
   return (
-  
+    
+    <Provider store={store}>
     <Router>
     <Fragment>
       
@@ -36,6 +38,7 @@ const App= ()=> {
       
     </Fragment>
     </Router>
+    </Provider>
   );
 }
 
