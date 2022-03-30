b3fcf01ae9ac1b9c63e08938f89497dd90a6218b
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-03-30 16:59:43 +0500

    1 file modified, 1 added

---
+++ src/components/profile/Articleitem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ArticleItem = () => {
+
+return(
+  <div className='row'>
+    <div className='col s2 m7'>
+)
+
+
+
+}
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,30 +3,38 @@
 import React, { Fragment } from 'react'
 
  const Profile = () => {
-  return (
+  return ( 
     <Fragment>
       <div className='row'>
-        <div className='col s6'>
+       <div className='row center-align'>
+        <div className='col s5 m7'>
         
-  <i class="m material-icons">
+  <i class="l material-icons">
     add
   </i>
   </div>
-  <div className='col s5'>
+  <div className='col s7 m4 '>
         <p>hi my is sijad khan i am developer </p>
         </div>
-  <h1> Social Links </h1>
+        </div>
+     <div className='devider center-align'>   
+     <div className='section'>
+        <div className='row'>
+  <h4> Social Links </h4>
       <div className='col s2'>
     <a href=''></a>
         </div>
-        <div className='col s3'>Facebook</div>
-        <div className='col s3'>Instagram</div>
-        <div className='col s3'>GITHUB</div>
-        <div className='col s3'>twitter</div>
-
+        <div className='col s2'>Facebook</div>
+        <div className='col s2'>Instagram</div>
+        <div className='col s2'>GITHUB</div>
+        <div className='col s2'>twitter</div>
+       </div>
+       </div>
+       </div>
     <div className='devider'>
       <div className='section'>
-    <h>Articles</h3>
+    <h4 className='center-align'>Articles</h4>
+    
     
         </div>
           </div>
