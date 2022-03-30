cdb81521fcfb64e124b1d58744810448e3aa1df8
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-03-30 15:53:36 +0500

    profile.js components updated

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,14 +5,31 @@ import React, { Fragment } from 'react'
  const Profile = () => {
   return (
     <Fragment>
-      <div className='row s5'>
+      <div className='row'>
+        <div className='col s6'>
         
-        <i class="m material-icons">insert_chart</i>
+  <i class="m material-icons">
+    add
+  </i>
+  </div>
+  <div className='col s5'>
         <p>hi my is sijad khan i am developer </p>
-        
         </div>
-    <div className='purple accent-4 row s12'>
-      <p>hi my is sijad khan i am developer </p>
+  <h1> Social Links </h1>
+      <div className='col s2'>
+    <a href=''></a>
+        </div>
+        <div className='col s3'>Facebook</div>
+        <div className='col s3'>Instagram</div>
+        <div className='col s3'>GITHUB</div>
+        <div className='col s3'>twitter</div>
+
+    <div className='devider'>
+      <div className='section'>
+    <h>Articles</h3>
+    
+        </div>
+          </div>
 
     </div>
     </Fragment>
