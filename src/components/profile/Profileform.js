5beaf2b8fc7555f68b79f2254b3ff91e53840333
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-03-31 07:32:03 +0500

    profileForm Component updated

--- src/components/profile/Profileform.js
+++ src/components/profile/Profileform.js
@@ -6,67 +6,54 @@ import React,{useState} from 'react'
   const [profile,setProfile] = useState({
     
     bio: '',
+    skills: '',
     facebook:'',
     twitter: '',
     github: '',
     instagram:''
   })
 
-  const {bio,facebook,twitter,github,instagram} = profile
+  const {bio,skills,facebook,twitter,github,instagram} = profile
 
   const onChange = e => setProfile({...profile,[e.target.name]: e.target.value})
   return (
-    <div className='container'>
+    <div className='/*form-container*/'>
 
-    <i className="large material-icons">add_a_photo
+
+
+    <h2>Create Profile</h2>
+    <form /*onSubmit={onSubmit} */ >
+        <i className="large material-icons">add_a_photo
 </i>
 
-    <div className="row">
- <form className="col s12">
-   <div className="row">
-     <div className="input-field col s9">
-       <textarea id="textarea1" class="materialize-textarea">{bio}</textarea>
-       <label htmlFor="textarea1">Write Your bio</label>
-     </div>
-   </div>
-
-   <div className="row">
-    <div className="input-field col s6">
-      <input value="Alvin" id="first_name2" type="text" class="validate"/>
-      <label className="active" for="first_name2">Enter your Skills</label>
-    </div>
-  </div>
-
-  <div className="row">
-    <div className="input-field col s6">
-      <input value={facebook} id="first_name2" type="text" class="validate" onchange={onChange}/>
-      <label className="active" for="first_name2">Facebook link </label>
-    </div>
-    <div class="row">
-    <div class="input-field col s6">
-      <input value={twitter} id="first_name2" type="text" className="validate" onchange={onChange}/>
-      <label class="active" for="first_name2">Twitter link</label>
-    </div>
-  </div>
-  </div>
-  <div className="row">
-    <div className="input-field col s6">
-      <input value={github} id="first_name2" type="text" class="validate" onchange={onchange}/>
-      <label className="active" htmlFor="first_name2">Gihub link</label>
-    </div>
-    <div class="row">
-    <div class="input-field col s6">
-      <input value={instagram} id="first_name2" type="text" class="validate" onchange={onChange}/>
-      <label class="active" for="first_name2">Instagram link</label>
-    </div>
-  </div>
-  </div>
-
-  </form>
-        
-
+        <div className="form-group">
+            <label htmlFor="name">bio</label>
+            <input type="text" name="bio" value={bio} placeholder='Write About Yourself' onChange={onChange} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="skills">Add Skills</label>
+            <input type="text" name="skills" value={skills} placeholder='Add Skills' onChange={onChange} />
+        </div>
+        <div className="form-group">
+            <label htmlFor="password">Facebook Link</label>
+            <input type="password" name="facebook" value={facebook} placeholder='Facebook Link' onChange={onChange} minLength="6"/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="twitter">Confirm Password</label>
+            <input type="password" name="twitter" value={twitter} placeholder='Twitter Link' onChange={onChange} minLength="6" />
+        </div>
+          <div className="form-group">
+            <label htmlFor="twitter">Confirm Password</label>
+            <input type="password" name="twitter" value={twitter} placeholder='Twitter Link' onChange={onChange} minLength="6" />
+        </div>
+          <div className="form-group">
+            <label htmlFor="twitter">Confirm Password</label>
+            <input type="password" name="twitter" value={twitter} placeholder='Twitter Link' onChange={onChange} minLength="6" />
+        </div>
+        <input type="submit" value="Create" className="btn btn-success block" />
+    </form>
 </div>
-</div>
+
   )
 }
 
