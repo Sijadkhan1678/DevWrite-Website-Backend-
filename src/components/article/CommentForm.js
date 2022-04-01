a604994946e6910246f4977b079d0c5e0972600f
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-04-01 08:25:05 +0500

    Updated CommentForm component

--- src/components/article/CommentForm.js
+++ src/components/article/CommentForm.js
@@ -1,24 +1,58 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addComment } from '../../actions/articleActions';
+import { Comment } from '../../actions/articleActions';
+import  {setAlert} from '../../actions/alertActions'
+import {Redirect} from 'react-router-dom'
+  
+  
 
 
+const CommentForm = ({comment,auth:{isAuthenticated} }) => {
+  
+    const [comment, seComment] = useState('');
+    const onSubmit = () => {
+      if(comment ==='' ){
+    setAlert('comment should not be empty','danger')
+  }else{
+    isAuthenticated ? comment(comment) : <Redirect to='/register'/>
+  }
 
-const CommentForm = ({ }) => {
-    const [text, setText] = useState('');
+}
 
     return (
-      
+      <div className='row'>
+    
+      <div className='col s6 m6 l6'>
+      <textarea
+                name='comment'
+                cols='30'
+                rows='5'
+                placeholder='Write something here'
+                value={comment}
+                onChange={e => setComment(e.target.value)}
+                required
+              />
+      </div>
+      <div className='col s3 m4'>
+ <button onClick={onSubmit}>   <i   class="material-icons purple-text accent-4">
+      send
+      </i> </button>
+      </div>
+  
+      </div>
       )
   
 }
+const mapStateProps = state => ({
+  auth: state.auth
+})
 
 CommentForm.propTypes = {
-  addComment: PropTypes.func.isRequired
+  comment: PropTypes.func.isRequired
 };
 
 export default connect(
-  null,
-  { addComment }
+  mapStateProps,
+  { comment }
 )(CommentForm);
