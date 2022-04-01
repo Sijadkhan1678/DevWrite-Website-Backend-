04d8cc78a6d482e09d477589bdff81edc44262e9
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-04-01 09:26:45 +0500

    Updated Comment comment

--- src/components/article/Comment.js
+++ src/components/article/Comment.js
@@ -1,30 +1,40 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
 import { unComment } from '../../actions/articleActions';
 
 
-
-const CommentItem = ({comment,unComment})  => {
+const CommentItem = ({comment,unComment,auth})  => {
   const {_id,text,commentby,commentAt} = comment
+
+  
+  const onDelete = (articleId,_id) => {
+   unComment(_id,articleId)
+}
+
   return(
     <Fragment>
          <Link > 
               <div className="col s12 m9 l10">
-                <div className="card horizontal">
+                <div className="card">
                   <div class="card-image">
                     <img src={commentby.photo} className="responsive-img round-img" />
                     <span className='block'> {commentby.name} </span>
+                    <span>{new Date(commentAt).toDateString()} </span>
                   </div>
                   
                   <div class="card-content">
                       
-                <p>posted by {text}</p>
+                <p>{text}</p>
                   </div>
+                  <div className='card-action'>
+      {auth.user_id === commentby._id && (
+    <i onClick={onDelete} className='material-icons red-text'>delete</i>
+      )}
                   
+                  
+                 </div>
                  </div>
                  </div>
                  </Link>
@@ -34,7 +44,7 @@ const CommentItem = ({comment,unComment})  => {
 }
 
 CommentItem.propTypes = {
-  postId: PropTypes.string.isRequired,
+  articleId: PropTypes.string.isRequired,
   comment: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
   unComment: PropTypes.func.isRequired
