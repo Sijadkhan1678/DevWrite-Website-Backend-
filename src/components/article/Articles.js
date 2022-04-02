f02573200d4e6e1519e45626a4f38b3293c93e34
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-04-02 23:21:04 +0500

    Created Articles and Preloader Components

---
+++ src/components/article/Articles.js
@@ -0,0 +1,42 @@
+import React,{Fragment,useEffect} from 'react';
+import ArticleItem from './ArticleItem'
+import {getArticles} from '../../actions/articleActions';
+import Preloader from '../layout/Preloader';
+
+const Articles = ({Articles,getArticles}) => {
+ 
+ useEffect(()=>{
+   getArticles()
+ },[articles])
+ 
+
+  return (
+    <Fragment>
+    {
+    loading && article==null ? <Preloader/>:
+    articles.map(article => <ArticleItem key={article._id} article={article}/>)
+      
+    }
+  
+   </Fragment>
+
+
+
+  )
+
+}
+
+Articles.propTypes={
+  articles : propTypes.object.isRequired,
+  getArticles: propTypes.func.isRequired,
+  loading : propTypes.bool.isRequired
+}
+const mapStateToProps = state => ({
+  articles: state.articles,
+  loading: state.loading
+})
+
+export default connect(
+  mapStateToProps,
+  { getArticles}
+)(Articles);
