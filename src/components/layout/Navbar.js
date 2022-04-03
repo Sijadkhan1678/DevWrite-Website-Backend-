281a4ba8a6e4091c69f841bef892c4166f1fe566
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-04-03 09:15:15 +0500

    Updated Navbar.js Component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {connect} from 'react-redux'
 
 
-const Navbar = () =>{
+const Navbar = ({auth}) =>{
+  const {isAuthenticated,user} =auth
+   const authLinks = (
+        <Fragment>
+            <li>
+        <Link to='/profile'>
+   {  user && user.photo ? <img src={user.photo} className='responsive-img round-img' /> : <i className='material-icons-outlined'>account_circle</i>  }
+        </Link> </li>
+            <li>
+                <a onClick={onLogout} href="#!">
+                    <i className='material-icons-outlined'>logout</i>
+                    <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    );
+    
+    const guestLinks = (
+      <Fragment>
+                <li>
+                    <Link to='/register'>Register</Link>
+                </li>
+                <li>
+                    <Link to='/login'>Login</Link>
+                </li>
+            </Fragment>
+    )
 
     return(
 
@@ -11,15 +38,19 @@ const Navbar = () =>{
           <Link to='/Home'  className="brand-logo py-2">DevWrite</Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
 
-            <li><Link to='/Home'> Home </Link></li>
-
-            <li> <Link to='/login'> Login </Link></li>
-
-            <li><Link to='/register'> Register </Link></li>
+   {isAuthenticated ? authLinks :guestLinks}
           </ul>
         </div>
       </nav>
     )
 }
 
-export default Navbar;
+Navbar.propTypes={
+  auth: propTypes.object.isRequired
+}
+const  mapStateToProps = state =>{
+ auth: state.auth
+ 
+}
+
+export default connect(mapStateToProps,null)(Navbar);
